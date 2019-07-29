78f3df5c596d4c536ef219b271f07895fddd0d64
Rachael Turner <rachael.turner1@holbmac0020.bc.jsplc.net>
2019-07-29 14:55:08 +0100

    terrafore script

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Repurposed for Terraform learning.
 Aims:
 1) create GitHub project with CircleCI deployment pipeline & terraform scripts
 2) pipeline will run terraform scripts to create AWS resource & apply right permissions
-3) upload HTML to AWS S3.
+3) upload HTML to AWS S3
---
+++ example.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  profile    = "rands-nonprod_admin"
+  region     = "eu-west-2"
+}
+
+# Create AWS S3 bucket
+resource "aws_s3_bucket" "b" {
+  bucket = "rt-terraform-test-bucket"
+  acl    = "private"  # grantees and permissions to apply
+  tags = {
+    Name        = "terraform bucket"
+    Environment = "Dev"
+  }
+  versioning {
+      enabled = true
+  }
+}
+
+# Upload file to bucket
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket = aws_s3_bucket.b.bucket
+  key = "helloWorld.html"  # object name
+  source = "./helloWorld.html"  # path to file to upload
+  content_type = "text/html"
+  # etag allows Terraform to recognise file changes
+  etag = "${filemd5("./helloWorld.html")}"
+}
